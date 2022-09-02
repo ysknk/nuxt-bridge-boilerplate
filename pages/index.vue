a0e8e46206a06ef8972d374de14c58c8a81af630
ysknk <31568245+ysknk@users.noreply.github.com>
2022-09-02 22:28:29 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -21,20 +21,20 @@ onMounted(() => {
 </script>
 
 <template lang="pug">
-  div
-    p {{ foo.state.value.hoge }}
-    p {{ foo.state.value.huga }}
-    p {{ foo.getState() }}
-    p {{ env }}
-    p width: {{ $window.width }}
-    p height: {{ $window.height }}
-    p scrolltop: {{ $window.scrollTop }}
+div
+  p {{ foo.state.value.hoge }}
+  p {{ foo.state.value.huga }}
+  p {{ foo.getState() }}
+  p {{ env }}
+  p width: {{ $window.width }}
+  p height: {{ $window.height }}
+  p scrolltop: {{ $window.scrollTop }}
 
-    p
-      AtomsLink(to="/foo") foo
-    p
-      AtomsLink(to="https://example.com" target="_blank") example
+  p
+    AtomsLink(to="/foo") foo
+  p
+    AtomsLink(to="https://example.com" target="_blank") example
 
-    MoleculesTutorial
+  MoleculesTutorial
 </template>
 
