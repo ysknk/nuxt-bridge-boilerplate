5a57e19fd4118287934fc17f7b67d7812ca95685
ysknk <31568245+ysknk@users.noreply.github.com>
2022-10-17 16:29:00 +0900

    Update useSEO.ts

--- composables/useSEO.ts
+++ composables/useSEO.ts
@@ -11,21 +11,21 @@ export const useSEO = (payload: TState) => {
 
   const meta = []
   if (payload.title) {
-    meta.push({ hid: 'og:title', name: 'og:title', content: payload.title })
+    meta.push({ hid: 'og:title', property: 'og:title', content: payload.title })
   }
   if (payload.url) {
     meta.push({ hid: 'canonical', name: 'canonical', content: payload.url })
-    meta.push({ hid: 'og:url', name: 'og:url', content: payload.url })
+    meta.push({ hid: 'og:url', property: 'og:url', content: payload.url })
   }
   if (payload.description) {
     meta.push({ hid: 'description', name: 'description', content: payload.description })
-    meta.push({ hid: 'og:description', name: 'og:description', content: payload.description })
+    meta.push({ hid: 'og:description', property: 'og:description', content: payload.description })
   }
   if (payload.keywords) {
     meta.push({ hid: 'keywords', name: 'keywords', content: payload.keywords.join(',') })
   }
   if (payload.type) {
-    meta.push({ hid: 'og:type', name: 'og:type', content: payload.type })
+    meta.push({ hid: 'og:type', property: 'og:type', content: payload.type })
   }
 
   useNuxt2Meta({
